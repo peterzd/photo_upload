4bb3664dd58c17a4f93527f0ee37625aafc2364d
peterzd.work <zdsunshine0640@126.com>
2013-05-14 10:49:28 +0800

    init env

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'pg'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'factory_girl_rails'
+  gem 'rspec-rails'
+  gem 'faker'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UploadShare::Application.config.secret_token = '85bf986874c68c4030d2eb300e97e4d25e80463309e193df2b0beab54d1f9f67892dee1100cd995485a8db52d1d07af329358770ecf52abb24a7325889e3e1d7'
